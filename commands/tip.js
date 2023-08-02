fd0450a66d3bef26da4b557c90f5f86da528dcd6
ClaireWhere <clairewhere4@gmail.com>
2023-08-01 22:10:21 -0400

    Feat: add !tip command

---
+++ commands/tip.js
@@ -0,0 +1,10 @@
+module.exports = {
+    data: {
+        name: 'tip',
+        description: `sends AAAAurora_'s Streamlabs tip link`,
+        defaultPermissions: false
+    },
+    execute(client, channel, tags, args) {
+        client.say(channel, `Any support is appreciated! https://streamlabs.com/aaaaurora_/tip`);
+    }
+}
