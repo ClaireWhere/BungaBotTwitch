3ba42713318fa75daed992797db0e66ec8bf4d25
ClaireWhere <clairewhere4@gmail.com>
2023-12-24 13:06:48 -0500

    feat: add fc command
    Alias of switch command

---
+++ commands/fc.js
@@ -0,0 +1,8 @@
+import { data as _data, execute as _execute } from './switch.js';
+
+export const data = {
+    name: 'fc',
+    description: _data.description,
+    defaultPermissions: _data.defaultPermissions
+};
+export const execute = _execute;
